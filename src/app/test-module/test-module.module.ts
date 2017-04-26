3ea0a58788821b112ff50094810d98c2d6482b37
Paul Lockwood <dotnetchap@gmail.com>
2017-04-26 11:57:35 -0600

    ng g component my-new-component  - Did in new module's folder  - ng test works correctly

--- src/app/test-module/test-module.module.ts
+++ src/app/test-module/test-module.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [MyNewComponentComponent]
 })
 export class TestModuleModule { }
